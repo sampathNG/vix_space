e65fd357e4e12a30dacd851ae177c9e4d0924323
sampathNG <jnanisampath@gmail.com>
2022-03-19 09:34:55 +0530

    fencing file updated

--- day2/fencing.js
+++ day2/fencing.js
@@ -1,6 +1,4 @@
 const Polygon = require("polygon")
-// var Vec2 = require('vec2');
-// const turf = require("@turf/line-to-polygon").default()
 
 var {a,b,c,d,e,f,g,h,x} = require("./routes/routes")
 
@@ -10,21 +8,12 @@ var {a,b,c,d,e,f,g,h,x} = require("./routes/routes")
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 5,
-      center: { lat: 24.886, lng: -70.268 },
+      center: x[2],
       mapTypeId: "terrain",
     });
-    // Define the LatLng coordinates for the polygon's path.
-    // const triangleCoords = [
-    //   { lat: 25.774, lng: -80.19 },
-    //   { lat: 18.466, lng: -66.118 },
-    //   { lat: 32.321, lng: -64.757 },
-    //   { lat: 25.774, lng: -80.19 },
-    // ];
 
-    // const triangleCoords =  x
-
-    // Construct the polygon.
-    const bermudaTriangle = new google.maps.Polygon({
+    // Construct the geofencing
+    const geofencing = new google.maps.Polygon({
       paths: x,
       strokeColor: a,
       strokeOpacity: b,
@@ -36,5 +25,5 @@ function initMap() {
       visible: h
     });
 
-    bermudaTriangle.setMap(map);
+    geofencing.setMap(map);
   }
